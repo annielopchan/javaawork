529eb0a2c268a44fd188708db06034fef3a40979
Anne Lopchan <annielopchan07@gmail.com>
2025-11-19 09:50:41 +0545

    Initial sharing of project

---
+++ workshop3.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class workshop3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class workshop3
+{
+    int age; //instance variable
+    static int qty; //static variable
+    
+    public static void main(String[] args)
+    {
+        // <className> <variable>= new <className>();
+        int age; //local variable
+        
+        workshop3 v1= new workshop3(); //calling instance variable in static method
+        System.out.println(v1.age);
+        
+        System.out.println(age);
+        System.out.println(workshop3.qty);
+    }
+    
