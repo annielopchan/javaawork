4b59e445d36aa79b240b5f801b272e2bcca817a1
Anne Tamang Lopchan <annielopchan07@gmail.com>
2025-11-22 08:39:48 +0545

    Workshop 3

--- DefaultValue.java
+++ DefaultValue.java
@@ -6,7 +6,8 @@
  * @version (a version number or a date)
  */
 public class DefaultValue
-{
+{   
+    // Declare member variables (fields) for all 8 primitive types without initializing them.
     byte myByte;
     short myShort;
     int myInt;
@@ -17,8 +18,10 @@ public class DefaultValue
     boolean myBoolean;
     
      public static void main(String[] args) {
-     // Creatie an instance of the class
+     // Create an instance of the class
         DefaultValue obj = new DefaultValue();
+        
+     // Printing the value
         System.out.println("Byte: " + obj.myByte);
         System.out.println("Short: " + obj.myShort);
         System.out.println("Int: " + obj.myInt);
@@ -28,10 +31,10 @@ public class DefaultValue
         System.out.println("Char: '" + obj.myChar + "'");
         System.out.println("Boolean: " + obj.myBoolean);
         
-        /*
-         * Fields in a class get default values automatically.
-         * However, local variables inside a method do not get default value.
-         * If you create a local variable in main and don't initialize it, the program won't compile because Java needs local variables to have a valur before use.
-         */
+     /*These default values apply only to class fields.
+     * Local variables inside methods do not receive any default value.
+     * If you create a local variable but don’t assign something to it,
+     * Java will show a compile-time error because it must be initialized before it can be used.
+     */
     }
 }
